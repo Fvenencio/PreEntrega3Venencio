4c90ea2417888e5db88486eed2861b0cea42acb2
Fabrizio Venencio <fvenencio@gmail.com>
2023-06-29 15:18:44 -0300

    correcciones de la primer entrega y realizacion de la segunda

--- JS/app.js
+++ JS/app.js
@@ -1,9 +1,3 @@
-function imprimirNumerosHasta(n) {
-  for (let i = 0; i <= n; i++) {
-    console.log(i);
-  }
-}
-
 function calcularEdadPromedio(edades) {
   let suma = 0;
 
@@ -16,14 +10,56 @@ function calcularEdadPromedio(edades) {
 }
 
 let edades = [];
+let cantidad;
 
-let cantidad = prompt("Ingrese la cantidad de personas:");
+do {
+  cantidad = parseInt(prompt("Ingrese la cantidad de personas:"));
 
-for (let i = 0; i < cantidad; i++) {
+  if (cantidad < 1 || cantidad > 10000 || isNaN(cantidad)) {
+    alert("La cantidad ingresada no es válida. Debe ser un número entre 1 y 10000.");
+  }
+} while (cantidad < 1 || cantidad > 10000 || isNaN(cantidad));
+
+let i = 0;
+while (i < cantidad) {
   let edad = parseInt(prompt(`Ingrese la edad de la persona ${i + 1}:`));
-  edades.push(edad);
+
+  switch (true) {
+    case isNaN(edad):
+      alert("La edad ingresada no es un número válido.");
+      break;
+    case edad < 1:
+      alert("La edad ingresada no puede ser menor a 1.");
+      break;
+    case edad > 1000:
+      alert("La edad ingresada no puede ser mayor a 1000 años.");
+      break;
+    default:
+      edades.push(edad);
+      i++; 
+  }
 }
 
-console.log(calcularEdadPromedio(edades));
-let edadPromedio = calcularEdadPromedio(edades);
-prompt("La edad promedio de las personas registradas es: " + edadPromedio);
+if (edades.length === cantidad) {
+  let edadPromedio = calcularEdadPromedio(edades);
+  
+  let resultadoPromedio = document.createElement("p");
+  resultadoPromedio.textContent = "La edad promedio de las personas registradas es de: " + edadPromedio + "años";
+
+  let resultadoCantidad = document.createElement("p");
+  resultadoCantidad.textContent = "Cantidad de personas registradas: " + cantidad;
+
+  let listaEdades = document.createElement("ul");
+  for (let j = 0; j < edades.length; j++) {
+    let itemEdad = document.createElement("li");
+    itemEdad.textContent = "Edad persona " + (j + 1) + ": " + edades[j] + "años" ;
+    listaEdades.appendChild(itemEdad);
+  }
+
+  let lista = document.createElement("div");
+  lista.appendChild(resultadoCantidad);
+  lista.appendChild(listaEdades);
+  lista.appendChild(resultadoPromedio);
+
+  document.body.appendChild(lista);
+}
